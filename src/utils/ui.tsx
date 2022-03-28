2b4cee340a7bd5ab957e0d2a561af0b37ffaad47
Dino <juntaek@owin.kr>
2022-03-28 20:44:38 +0900

    1. utils > ui 파일 수정, 2. 절대경로 추가

--- src/utils/ui.tsx
+++ src/utils/ui.tsx
@@ -1,13 +1,22 @@
 import { useMediaQuery } from "react-responsive";
 
-export const isPc = useMediaQuery({
-  query: "(min-width:1024px)",
-});
+const IsPc = () =>
+  useMediaQuery({
+    query: "(min-width:1024px)",
+  });
 
-export const isTablet = useMediaQuery({
-  query: "(min-width:768px) and (max-width:1023px)",
-});
+const IsTablet = () =>
+  useMediaQuery({
+    query: "(min-width:768px) and (max-width:1023px)",
+  });
 
-export const isMobile = useMediaQuery({
-  query: "(max-width:767px)",
-});
+const IsMobile = () =>
+  useMediaQuery({
+    query: "(max-width:767px)",
+  });
+
+export default {
+  IsPc,
+  IsTablet,
+  IsMobile,
+};
